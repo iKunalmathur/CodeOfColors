4763337f8dd58bc1fe525e4ce1bdabd6232f5d1e
Kunal Mathur <Iamkunal2k@gmail.com>
2020-10-27 11:37:02 +0530

    coc v2

---
+++ precache-manifest.3a73b6e052ac1f2aa872929bd6419ed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591f4c04c4f28967972dfa903c397aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d764b217319f91f5ce28",
+    "url": "/static/css/main.cabd7464.chunk.css"
+  },
+  {
+    "revision": "c44d8efff9f8cbe88035",
+    "url": "/static/js/2.37151e20.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.37151e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d764b217319f91f5ce28",
+    "url": "/static/js/main.3621d881.chunk.js"
+  },
+  {
+    "revision": "4aecdf32aa2711ebc5e3",
+    "url": "/static/js/runtime-main.705bb93a.js"
+  }
+]);
